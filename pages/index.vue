e0cc628ee915563099a9788c586c7718a77da56d
KiwSh <rifqidwiputra05@gmail.com>
2023-12-01 10:40:53 +0700

    push tugas akhir smkcoding

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <main>
+
+    <div class="body">
+    <h1>Fast List</h1>
+    <p>Catat Kegiatanmu Disini!!</p>
+   </div>
+
+    <div class="create-new">
+      <input 
+        type="text" 
+        v-model="newTask" 
+        placeholder="" 
+        @keypress.enter="addTask" />
+      <button class="comic-button" @click="addTask">Tambah</button>
+    </div>
+
+   
+    
+  
+
+    
+
+
+    <div class="tasks">
+      <Task
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" />
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  data (){
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
